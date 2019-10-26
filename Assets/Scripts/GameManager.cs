9c78893d947ec0315f7be00e2476bee62fd16697
Lorenzo Ventura <lorenzo3.ventura@mail.polimi.it>
2019-10-26 12:36:21 +0200

    Project Rebuild
    -Created  a new prject from scratch with Progrids and Probuilder working (at least for me)
    - Added PolimiCollective for useful scripts

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    public GameObject Cube;
+
+
+
+    // public Screen screen;
+
+    // private GameObject _player;
+
+
+    void Awake()
+    {
+        ObjectPoolingManager.Instance.CreatePool(Cube, 100, 200);
+
+
+    }
+
+    void Start()
+    {
+
+        FirstLevel();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void FirstLevel()
+    {
+        /*if (_player == null)
+        {
+            _player = Instantiate(Player, Vector3.zero, Quaternion.identity);
+            _player.SetActive(true);
+            GameObject go = ObjectPoolingManager.Instance.GetObject("Ufo");
+            go.transform.position = new Vector2(5, 5);
+            //            go.transform.position = screen.Random();
+        }*/
+    }
+
+}
---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("g"))
+        {
+            StartCoroutine("Fade");
+        }
+    }
+    IEnumerator Fade()
+    {
+        for (float ft = 1f; ft >= 0; ft -= 0.1f)
+        {
+            GameObject cube= ObjectPoolingManager.Instance.GetObject("Cube");
+            cube.transform.position = new Vector2(0,5);
+            yield return new WaitForSeconds(3f);
+        }
+    }
+}

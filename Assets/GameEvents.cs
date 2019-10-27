d2ac2380e5d8ed61bd7c37b9c7c99c5b2d886b8e
Lorenzo Ventura <lorenzo3.ventura@mail.polimi.it>
2019-10-27 23:05:49 +0100

    Added more complex details
    -Event system
    -Time swap with events
    - runtime CubeSet

---
+++ Assets/GameEvents.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvents : MonoBehaviour
+{public static GameEvents current;
+    public event Action onTimeStop;
+
+    public void TimeStop()
+    {
+        if (onTimeStop != null)
+        {
+            onTimeStop();
+        }
+    }
+        private void Awake()
+    {
+        current = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

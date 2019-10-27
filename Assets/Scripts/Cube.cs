f38b4d0c43eda3f8139671fcb4b2899e254ed278
Lorenzo Ventura <lorenzo3.ventura@mail.polimi.it>
2019-10-27 23:05:49 +0100

    Added more complex details
    -Event system
    -Time swap with events
    - runtime CubeSet

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public CubeSet cubeSet;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        GameEvents.current.onTimeStop += switchToKinematic;
+//        GameEvents.current.onTimeStop += ;
+
+        rb = GetComponent<Rigidbody>();
+        if (GameManager.Instance.time == true)
+        {
+            rb.isKinematic = false;
+        }
+        else
+        {
+            rb.isKinematic = true;
+        }
+    }
+    private void switchToKinematic()
+    {
+        if (rb.isKinematic == true)
+        {
+            rb.isKinematic = false;
+        }
+        else
+        {
+            rb.isKinematic = true;
+        }
+    }
+    private void OnEnable()
+    {
+cubeSet.Add(this);        
+    }
+    private void OnDisable()
+    {
+        cubeSet.Remove(this);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Input.GetKeyDown("c"))
+        {
+            rb.AddForce(0,600,0);
+        }*/
+    }
+}

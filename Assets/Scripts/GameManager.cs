435ede786d07b34bc5ae90fbeab4ce3757d06360
Lorenzo Ventura <lorenzo3.ventura@mail.polimi.it>
2019-10-29 11:55:28 +0100

    fixed bug about force to apply during stop

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,9 +9,10 @@ public class GameManager : Singleton<GameManager>
     public bool time=true;
     [Header("Sets handling")]
     public CubeSet cubeSet;
-    [Header("Others")]
     private List<Vector3> forceToApply;
+    [Header("Obj Generator")]
     public GameObject objPrefab;
+    public int times = 1;
     public int quantity = 1;
     public Transform spawnPosition;
     private List<GameObject> cubeList = new List<GameObject>();
@@ -48,6 +49,7 @@ public class GameManager : Singleton<GameManager>
         if (Input.GetKeyDown("t")){
             timeSwitch();        }
     }
+
     void timeSwitch()
     {
         GameEvents.current.TimeChange();
@@ -58,26 +60,29 @@ public class GameManager : Singleton<GameManager>
         else
         {
             time = true;
+            int count = 0;
 
+            foreach (var go in cubeList)
+            {
+                go.GetComponent<Rigidbody>().AddForce(forceToApply[count]);
+                forceToApply[count] = new Vector3(0, 0, 0);
+                count++;
+
+            }
 
         }
 
-        int count=0;
 
-        foreach (var go in cubeList)
-        {
-            go.GetComponent<Rigidbody>().AddForce(forceToApply[count]);
-            count++;
-
-        }
+       
 
     }
     IEnumerator SpawnObjects()
     {
-        for (float ft = 1f; ft >= 0; ft -= 0.1f)
+        for (int j=0; j< times;j++)
         {
             for (int i = 0; i < quantity; i++)
             {
+                forceToApply.Add(new Vector3(0,0,0));
                 GameObject obj = ObjectPoolingManager.Instance.GetObject(objPrefab.name);
                 Vector3 basePosition = spawnPosition.position;
                 Vector3 CasualAdd = new Vector3(Random.Range(-10, 10), 20, Random.Range(-5, 5));
@@ -98,10 +103,14 @@ public class GameManager : Singleton<GameManager>
     {
         if (Input.GetKeyDown("z"))
         {
+            int count1 = 0;
             Debug.Log(cubeList.Count);
                foreach (var go in cubeList)
            {
-                if (time == false) { forceToApply.Add(new Vector3(0, 600, 0)); }
+                if (time == false) {
+                    forceToApply[count1] = forceToApply[count1] + new Vector3(0, 600, 0);
+                    count1++;
+                     }
                 else
                 {
                     go.GetComponent<Rigidbody>().AddForce(0, 600, 0);

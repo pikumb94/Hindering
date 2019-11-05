1cc0522a7c4ea88dc8252b6029a2396b639346ec
pikumb <pikumb94@gmail.com>
2019-11-05 09:52:01 +0100

    Added some basic line rendering (still some bugs to fix)

--- Assets/Scripts/IndicatorMouseFollow.cs
+++ Assets/Scripts/IndicatorMouseFollow.cs
@@ -9,6 +9,7 @@ public class IndicatorMouseFollow : MonoBehaviour
     float distanceToPlane;
     Vector3 dst;
     Plane plane;
+    LineRenderer lineRenderer;
     public float hitRadius = 1f;
     public float forceMagnitude = 1f;
     CapsuleCollider coll;
@@ -40,10 +41,62 @@ public class IndicatorMouseFollow : MonoBehaviour
                
                 hit.rigidbody.AddForceAtPosition(dst.normalized* forceMagnitude, hit.point, ForceMode.Impulse);
                 Debug.DrawRay(new Vector3(transform.position.x, transform.position.y, 0), dst*0.5f, Color.red, 1);
+
+                if (hit.transform.gameObject.GetComponent<LineRenderer>())
+                {
+                    lineRenderer.SetPosition(0, hit.point);
+                    lineRenderer.SetPosition(1, 2 * dst);
+                }
+                else
+                {
+                    lineRenderer = hit.transform.gameObject.AddComponent<LineRenderer>();
+                    lineRenderer.useWorldSpace = false;
+                    lineRenderer.startWidth = 0.01f;
+                    lineRenderer.endWidth = 0.2f;
+                    lineRenderer.startColor = Color.red;
+                    lineRenderer.endColor = Color.red;
+                    lineRenderer.SetPosition(0, hit.transform.position- hit.point );
+                    lineRenderer.SetPosition(1, 2 * dst);
+                }
+
             }
             Debug.Log(hit.transform.name);
         }
-        
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.DrawRay(transform.position, dst, Color.green, 1);
+            RaycastHit hit;
+
+            if (Physics.Raycast(new Vector3(transform.position.x, transform.position.y, 0), dst, out hit, hitRadius))
+            {
+
+                hit.rigidbody.AddForceAtPosition(dst.normalized * forceMagnitude, hit.transform.position, ForceMode.Impulse);
+                Debug.DrawRay(new Vector3(transform.position.x, transform.position.y, 0), dst * 0.5f, Color.red, 1);
+
+                if (hit.transform.gameObject.GetComponent<LineRenderer>())
+                {
+                    lineRenderer.SetPosition(0, Vector3.zero);
+                    lineRenderer.SetPosition(1, 2 * dst);
+                }
+                else
+                {
+                    lineRenderer = hit.transform.gameObject.AddComponent<LineRenderer>();
+                    lineRenderer.useWorldSpace = false;
+                    lineRenderer.startWidth = 0.01f;
+                    lineRenderer.endWidth = 0.2f;
+                    lineRenderer.startColor = Color.red;
+                    lineRenderer.endColor = Color.red;
+                    lineRenderer.SetPosition(0, Vector3.zero);
+                    lineRenderer.SetPosition(1, 2 * dst);
+                }
+                
+
+            }
+            Debug.Log(hit.transform.name);
+        }
+
+
     }
 
 

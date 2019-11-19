cd8ee39660c1017b7069aeffdb3f599b5f8cb7ab
Luxspark <lux.spark@libero.it>
2019-11-20 00:25:56 +0100

    Magnet Object Complete

--- Assets/Scripts/Magnet.cs
+++ Assets/Scripts/Magnet.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class Magnet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public float charge;
+  public float maxForce;
+  public Vector3 PointZeroRot;
+  public GameObject pivot;
+  public float stdMoment;
+  float dis;
+  float force;
+  float torque;
+  Vector3 disVec;
+  Vector3 normDis;
+  Vector3 rotation;
+  Quaternion deviation;
+  Vector3 rotAxis;
+//  Vector3 forbiddenRot;
+  Rigidbody rb;
+  Rigidbody otherRb;
 
-    // Update is called once per frame
-    void Update()
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  void OnTriggerStay(Collider other)
+  {
+    if(other.tag == "Metal")
     {
-        
+      disVec = other.transform.position - pivot.transform.position;
+      dis = Mathf.Sqrt(Vector3.Dot(disVec, disVec));
+      normDis = Vector3.Normalize(disVec);
+      rotation = Vector3.Normalize(transform.rotation * PointZeroRot);
+      deviation = Quaternion.FromToRotation(normDis, rotation);
+      torque = Mathf.Sqrt(Quaternion.Dot(deviation, deviation));
+      rotAxis = Vector3.Normalize(Vector3.Cross(rotation, normDis));
+      /*forbiddenRot = Quaternion.FromToRotation(PointZeroRot, rotation) * Vector3.up;
+      if(Mathf.Sqrt(Vector3.Dot(forbiddenRot - rotAxis, forbiddenRot - rotAxis)) != 0)
+      {
+        rotAxis = Vector3.Normalize(rotAxis - forbiddenRot);
+      }*/
+      otherRb = other.GetComponent<Rigidbody>();
+      force = charge / (dis);
+      if(force < maxForce)
+      {
+        rb.AddForce(normDis * force, ForceMode.Force);
+        rb.AddTorque(rotAxis * torque * stdMoment);
+        otherRb.AddForce(- normDis * force, ForceMode.Force);
+      }else
+      {
+        rb.AddForce(normDis * maxForce, ForceMode.Force);
+        rb.AddTorque(rotAxis * torque * stdMoment);
+        otherRb.AddForce(- normDis * maxForce, ForceMode.Force);
+      }
     }
+  }
+
 }

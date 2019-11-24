bf1130224a3aa1497cb8e69c3bca46e9873d3026
Luxspark <lux.spark@libero.it>
2019-11-24 18:44:33 +0100

    Lever now moves depending on forces applied to it

--- Assets/Scripts/LeverTrajObject.cs
+++ Assets/Scripts/LeverTrajObject.cs
@@ -2,56 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LeverTrajObject : TimeBehaviour
+public class LeverTrajObject : MonoBehaviour
 {
     public TrajObject so;
-    public GameObject stick;
     public GameObject pivot;
+    public GameObject sphere;
+    float pivotHeight;
     public bool on = false;
-    public bool time = true;
-    public float angleOn = 45f;
-    public float angleOff = 135f;
-    float angle = 135f;
-    float angleSpeed = 200f;
 
 
     void Start()
     {
-      base.Start();
+      pivotHeight = pivot.transform.position.y;
     }
 
     void Update()
     {
-      if (Input.GetKeyDown("f"))
+      if(sphere.transform.position.y - pivotHeight > 0)
       {
-          on = true;
-          so.backward = !(on);
-      }
-      if (Input.GetKeyDown("b"))
-      {
-          on = false;
-          so.backward = !(on);
-      }
-      if(time && on && angle > angleOn)
-      {
-        stick.transform.RotateAround(pivot.transform.position, Vector3.left, Time.deltaTime * angleSpeed);
-        angle -= Time.deltaTime * angleSpeed;
-      }
-      if(time && !(on) && angle < angleOff)
-      {
-        stick.transform.RotateAround(pivot.transform.position, Vector3.right, Time.deltaTime * angleSpeed);
-        angle += Time.deltaTime * angleSpeed;
-      }
-    }
-
-    protected override void swapTime()
-    {
-      if(time)
-      {
-        time = false;
+        on = false;
+        so.backward = !(on);
       }else
       {
-        time = true;
+        on = true;
+        so.backward = !(on);
       }
     }
 }

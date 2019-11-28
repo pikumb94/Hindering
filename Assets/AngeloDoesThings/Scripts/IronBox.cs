5736c13c00cb32138e7573d05a0a98cffd1c0038
Unknown <angelo.nasole@mail.polimi.it>
2019-11-28 15:23:24 +0100

    magnet on OLD(!!) player_RB
    new version incoming... però poi basta pls :)

--- Assets/AngeloDoesThings/Scripts/IronBox.cs
+++ Assets/AngeloDoesThings/Scripts/IronBox.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IronBox : MonoBehaviour
+public class IronBox : TimeBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        base.Start();
     }
 
     // Update is called once per frame

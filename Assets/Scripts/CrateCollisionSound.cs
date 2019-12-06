f9d87dcc092b19ee4a34726b7df67996d825f3e4
pikumb <pikumb94@gmail.com>
2019-12-06 09:29:19 +0100

    Crate sound script added

---
+++ Assets/Scripts/CrateCollisionSound.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrateCollisionSound : MonoBehaviour
+{
+
+    [FMODUnity.EventRef]
+    public string crateCollisionClipSoft = "";
+    [FMODUnity.EventRef]
+    public string crateCollisionClipHard = "";
+
+    public AudioSource source;
+    
+    private float velToVol = .2F;
+    private float velocityClipSplit = 10F;
+
+
+
+    void OnCollisionEnter(Collision coll)
+    {
+        float hitVol = coll.relativeVelocity.magnitude * velToVol;
+        if (coll.relativeVelocity.magnitude < velocityClipSplit)
+            FMODUnity.RuntimeManager.PlayOneShot(crateCollisionClipSoft, transform.position);
+        else
+            FMODUnity.RuntimeManager.PlayOneShot(crateCollisionClipHard, transform.position);
+    }
+
+}

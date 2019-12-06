559c389b91a0dcb9e1494a54099930b110ae6d73
Unknown <angelo.nasole@mail.polimi.it>
2019-12-06 18:57:59 +0100

    level skeleton

--- Assets/AngeloDoesThings/Scripts/NewMagnetRB.cs
+++ Assets/AngeloDoesThings/Scripts/NewMagnetRB.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewMagnetRB : MonoBehaviour
+public class NewMagnetRB : TimeBehaviour
 {
     
     //public GameObject _magnetWoop;
@@ -11,8 +11,12 @@ public class NewMagnetRB : MonoBehaviour
     public float boxForce = 10f;
     public float maxBoxForce = 100f;
     public float raycastDistance = Mathf.Infinity;
+    public bool time = false;
 
-
+    void Start()
+    {
+        base.Start();
+    }
     void Awake()
     {
         _body = transform.parent.parent.gameObject;
@@ -23,36 +27,40 @@ public class NewMagnetRB : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (time)
         {
-            RaycastHit hit;
-            int mask = (1 << 10);
-
-
-            if (Physics.Raycast((Vector2)transform.GetChild(0).transform.position, transform.up, out hit, raycastDistance, mask))
+            if (Input.GetMouseButton(0))
             {
-                Debug.DrawLine(hit.point, transform.GetChild(0).transform.position, Color.cyan);
+                RaycastHit hit;
+                int mask = (1 << 10);
 
-                //SpawnWoop(hit);
 
-                Vector3 dir = Vector3.Scale(transform.up, new Vector3(10, 1, 1));
+                if (Physics.Raycast((Vector2)transform.GetChild(0).transform.position, transform.up, out hit, raycastDistance, mask))
+                {
+                    Debug.DrawLine(hit.point, transform.GetChild(0).transform.position, Color.cyan);
 
-                //force moving player
-                _body.GetComponent<Rigidbody>().AddForce(dir * magneticForce * Time.deltaTime, ForceMode.Impulse);
+                    //SpawnWoop(hit);
 
-                //force moving box
-                hit.collider.GetComponent<ForceHandler>().addBaricentricForce(-transform.up, boxForce * Time.deltaTime, maxBoxForce);
+                    Vector3 dir = Vector3.Scale(transform.up, new Vector3(10, 1, 1));
+
+                    //force moving player
+                    _body.GetComponent<Rigidbody>().AddForce(dir * magneticForce * Time.deltaTime, ForceMode.Impulse);
+
+                    //force moving box
+                    hit.collider.GetComponent<ForceHandler>().addBaricentricForce(-transform.up, boxForce * Time.deltaTime, maxBoxForce);
+                }
+
             }
 
-        }
 
-
-        if (Input.GetKeyDown("1"))
-        {
-            GameObject punchMode = transform.parent.parent.GetChild(1).gameObject;
-            punchMode.SetActive(true);
-            transform.parent.gameObject.SetActive(false);
+            if (Input.GetKeyDown("1"))
+            {
+                GameObject punchMode = transform.parent.parent.GetChild(1).gameObject;
+                punchMode.SetActive(true);
+                transform.parent.gameObject.SetActive(false);
+            }
         }
+        
     }
 
     void SpawnWoop(RaycastHit hit)
@@ -62,4 +70,10 @@ public class NewMagnetRB : MonoBehaviour
         go.transform.rotation = transform.rotation;
     }
 
+    protected override void swapTime()
+    {
+        if (time) time = false;
+
+        else time = true;
+    }
 }

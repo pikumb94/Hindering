fd0073e82ff90498e61837c26bd2815f6848d551
Luxspark <lux.spark@libero.it>
2019-12-07 16:29:13 +0100

    menu

---
+++ Assets/Scripts/ButtonMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonMenu : Button
+{
+
+    public virtual void click()
+    {
+        Debug.Log("Button clicked");
+    }
+}
--- Assets/Scripts/MenuFunctions.cs
+++ Assets/Scripts/MenuFunctions.cs
@@ -3,17 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MenuFunctions :MonoBehaviour
-{
-    public Button start;
-    void Start()
-    {
-        start.onClick.AddListener(()=>startGame());
-    }
-    public void startGame()
-    {
-        Debug.Log("");
-       // SceneLoader.HandleSceneSwitch(SceneLoader.Scenes.Puzzle1);
+public class MenuFunctions : MonoBehaviour {
+	public ButtonMenu start;
 
-    }
+	void Start () {
+		ButtonMenu btn = start.GetComponent<ButtonMenu>();
+		btn.onClick.AddListener(btn.click);
+	}
 }

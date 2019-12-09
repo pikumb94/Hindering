52310433c835a362dd8b34e98e28112be1b26ea7
pikumb <pikumb94@gmail.com>
2019-12-09 14:12:53 +0100

    Added sound to the sliding door, minor sound fix

--- Assets/Scripts/DoorSliding.cs
+++ Assets/Scripts/DoorSliding.cs
@@ -15,56 +15,77 @@ public class DoorSliding : TimeBehaviour
     float zPos;
 
     [FMODUnity.EventRef]
-    public string doorOpeningSound =  "";
+    public string doorOpeningSound = "";
     [FMODUnity.EventRef]
     public string doorClosingSound = "";
+    private bool canPlay = false;
+
 
 
     void Start()
     {
-      base.Start();
-      zPos = door.transform.position.z;
+        base.Start();
+        zPos = door.transform.position.z;
     }
 
     void Update()
     {
-      if(time && backward)
-      {
-        FMODUnity.RuntimeManager.PlayOneShot(doorOpeningSound);
-          if (door.transform.position.z > zPos + maxMove)
-          {
-            rbDoor.velocity = Vector3.zero;
+        if (time && backward)
+        {
             
 
-          }else
-          {
-            rbDoor.velocity = new Vector3(0, 0, standard_speed);
-          }
-      }else if(time)
-      {
-        FMODUnity.RuntimeManager.PlayOneShot(doorClosingSound);
-          if (door.transform.position.z < zPos)
-          {
-            rbDoor.velocity = Vector3.zero;
+            if (door.transform.position.z > zPos + maxMove)
+            {
+                rbDoor.velocity = Vector3.zero;
+                canPlay = true;
 
-          }else
-          {
-            rbDoor.velocity = new Vector3(0, 0, -standard_speed);
-          }
+            }
+            else
+            {
+                rbDoor.velocity = new Vector3(0, 0, standard_speed);
 
-      }
+                PlaySound(doorOpeningSound);
+            }
+        }
+        else if (time)
+        {
+
+            if (door.transform.position.z < zPos)
+            {
+                rbDoor.velocity = Vector3.zero;
+                canPlay = true;
+            }
+            else
+            {
+                rbDoor.velocity = new Vector3(0, 0, -standard_speed);
+
+                PlaySound(doorClosingSound);
+            }
+
+        }
     }
 
     protected override void swapTime()
     {
 
-      if(time)
-      {
-        rbDoor.velocity = Vector3.zero;
-        time = false;
-      }else
-      {
-        time = true;
-      }
+        if (time)
+        {
+            rbDoor.velocity = Vector3.zero;
+            time = false;
+        }
+        else
+        {
+            time = true;
+        }
+    }
+
+    private void PlaySound(string path)
+    {
+        if (canPlay)
+        {
+            FMODUnity.RuntimeManager.PlayOneShot(path, GetComponent<Transform>().position);
+            canPlay = false;
+        }
+
     }
 }

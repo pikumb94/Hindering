8dd355cb9bb448120f05cc9c53414c17e70819e5
Lorenzo Ventura <lorenzo3.ventura@mail.polimi.it>
2019-12-24 12:56:15 +0100

    Fixed issue 9 and 16
    - added prefab with customizable monitor
    -slowed down running animation

---
+++ Assets/HelpText.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelpText : MonoBehaviour
+{
+    public String messageToDisplay;
+    public float typeSpeed=0.3f;
+    public float typeInitialDelay=2;
+    private Text textComponent;
+    public int clipsCount;
+    public List<AudioClip> typeClips= new List<AudioClip>();
+  
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (messageToDisplay == "")
+        {
+            messageToDisplay = "This is a monitor that aims to display some info for nub players ";
+        }
+        
+        textComponent = GetComponent<Text>();
+        StartCoroutine("writeMessage");
+        clipsCount = typeClips.Count;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator writeMessage()
+    {
+        yield return new WaitForSeconds(typeInitialDelay);
+
+        for (int i =0; i< messageToDisplay.Length+1;i++)
+        {
+           // int clipIndexToDisplay = i % clipsCount;
+           GetComponent<AudioSource>().PlayOneShot(typeClips[0]);
+            textComponent.text = messageToDisplay.Substring(0,i);
+            yield return new WaitForSeconds(typeSpeed);
+        }
+
+    }
+}

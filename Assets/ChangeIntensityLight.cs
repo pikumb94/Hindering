b1fa5893b901a767160db6a306601804f7e2bb54
PikUmb <pikumb94@gmail.com>
2019-12-30 18:31:22 +0100

    Tutorial1 refined

---
+++ Assets/ChangeIntensityLight.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeIntensityLight : MonoBehaviour
+{
+    private Light light;
+    private bool isPlayerInside=false;
+    float newIntensity;
+    float smoothTime = 1f;
+    float velocity1;
+    float velocity2;
+
+    private void Start()
+    {
+        light = GetComponent<Light>();
+        newIntensity = 0f;
+    }
+
+    private void Update()
+    {
+        if (isPlayerInside)
+        {
+            newIntensity = Mathf.SmoothDamp(light.intensity, 10f, ref velocity1, smoothTime);
+        }
+        else
+        {
+            newIntensity = Mathf.SmoothDamp(light.intensity, 0f, ref velocity2, smoothTime);
+        }
+        light.intensity = newIntensity;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        isPlayerInside = true;
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        isPlayerInside = false;
+    }
+}
---
+++ Assets/FollowDoorMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowDoorMovement : MonoBehaviour
+{
+    public Transform doorToFollow;
+    public bool isOneTimeOpened=false;
+    private float initPos;
+
+    private void Start()
+    {
+        initPos = transform.position.x;
+    }
+    void Update()
+    {
+        if (isOneTimeOpened) { 
+            if (initPos - transform.position.x >= 1.9f)
+                return;
+            else
+                transform.localPosition = new Vector3(doorToFollow.localPosition.x, transform.localPosition.y, transform.localPosition.z);
+        }
+        else
+        transform.localPosition = new Vector3(doorToFollow.localPosition.x, transform.localPosition.y, transform.localPosition.z);
+    }
+}

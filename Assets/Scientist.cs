1f059027ffb97c6d263c40c7693c7440fc6835c8
PikUmb <pikumb94@gmail.com>
2020-01-05 11:13:56 +0100

    Some little fix and animation control refined

--- Assets/Scientist.cs
+++ Assets/Scientist.cs
@@ -3,29 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Scientist : TimeBehaviour
-{private Animator _animator;
+{
+    private Animator _animator;
     private bool unlock = false;
+    public AnimationClip clip;
     // Start is called before the first frame update
+
     private new void Start()
     {
+        
         //base.Start();
         GameEvents.current.onTimeChange += swapTime;
 
         _animator = GetComponent<Animator>();
+
+        if(clip)
+            initAnimationClip();
+
         if (TimeHandler.Instance.time == true)
         {
             _animator.enabled = true;
         }
         else
         {
-            _animator.enabled = false;
-            //retardedStopAnimator();
+            //_animator.enabled = false;
+            StartCoroutine("retardedStopAnimator");
         }
 
     }
     private  IEnumerator retardedStopAnimator()
     {
-       yield return new  WaitForSeconds(1);
+       yield return new  WaitForSeconds(.1f);
         _animator.enabled = false;
 
 
@@ -48,6 +56,19 @@ public class Scientist : TimeBehaviour
         {
             _animator.enabled = true;
 
+        }
+    }
+
+    void initAnimationClip()
+    {
+        if (clip)
+        {
+            AnimatorOverrideController aoc = new AnimatorOverrideController(_animator.runtimeAnimatorController);
+            var anims = new List<KeyValuePair<AnimationClip, AnimationClip>>();
+            foreach (var a in aoc.animationClips)
+                anims.Add(new KeyValuePair<AnimationClip, AnimationClip>(a, clip));
+            aoc.ApplyOverrides(anims);
+            _animator.runtimeAnimatorController = aoc;
         }
     }
 }
